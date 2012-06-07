263bc54a8114c11b44d0d2df17d055474e3f8e2d
akramnik <akramnik@gmail.com>
2012-06-07 17:45:49 -0500

    WIP

--- SConstruct
+++ SConstruct
@@ -1,3 +1,3 @@
 env = Environment()
 env.Append(CCFLAGS = ['-std=c++0x'])
-env.Program(target='main', source=['main.cpp'])
+env.Program(target='main', source=['main.cpp'], CPPPATH = "/home/akramnik/Downloads/boost_1_49_0")
--- main.cpp
+++ main.cpp
@@ -3,38 +3,45 @@
 #include <tuple>
 #include <stdexcept>
 
+#include <boost/make_shared.hpp>
+
 template<typename Output, typename Target>
 class Queue {
     Target &target;
 public:
     Queue(Target &target) : target(target) { }
+
     void send(Output output) {
 	target.process(output);
+    }
+
+    void start() {
     }
 };
 
 template<typename Output>
-class Component {
+class OutputComponent {
 public:
     typedef Output OutputType;
     void onOutput(std::function<void(Output)> outputCallback) {
-	this->outputCallback = outputCallback;
+	_outputCallback = outputCallback;
     }
 private:
-    std::function<void(Output)> outputCallback;
+    std::function<void(Output)> _outputCallback;
 protected:
-    void send(Output output) { outputCallback(output); }
+    void send(Output output) { _outputCallback(output); }
 };
 
-class Reader : public Component<std::tuple<int, double, int, double>> {
+class Reader : public OutputComponent<std::tuple<int, double, int, double>> {
 public:
     void start() {
 	send(OutputType(3, 3.3, 5, 5.4));
     }
 };
 
-class Transform : public Component<std::tuple<int, double>> {
+class Transform : public OutputComponent<std::tuple<int, double>> {
 public:
+    void start() { }
     void process(std::tuple<int, double, int, double> tuple){
 	send(OutputType(std::get<0>(tuple) + std::get<2>(tuple),
 			std::get<1>(tuple) + std::get<3>(tuple)));
@@ -46,28 +53,51 @@ public:
     void process(std::tuple<int, double> tuple) {
 	std::cout << std::get<0>(tuple) << ", " << std::get<1>(tuple) << std::endl;
     }
+    void start() { }
 };
 
 class Pipeline {
+private:
+    std::vector<std::function<void()>> commands;
+
 public:
+    template<typename Type, typename...Args>
+    boost::shared_ptr<Type> create(Args...args) {
+	auto component = boost::make_shared<Type>(std::move(args)...);
+	commands.push_back([component] {
+		component->start();
+	    });
+	return component;
+    }
+
     template<typename Left, typename Right>
-    void connect(Left &left, Right &right) {
-	Queue<typename Left::OutputType, decltype(right)> queue(right);
-	left.onOutput([&queue, &right](typename Left::OutputType output) {
-		queue.send(output);
+    void connect(boost::shared_ptr<Left> &left, boost::shared_ptr<Right> &right) {
+	auto queue = boost::make_shared<Queue<typename Left::OutputType, Right>>(*right);
+	left->onOutput([queue, &right](typename Left::OutputType output) {
+		queue->send(output);
+	    });
+	commands.push_back([queue] {
+		queue->start();
+	    });
+    }
+
+    void start() {
+	std::for_each(commands.begin(), commands.end(), [] (const std::function<void()> &command) {
+		command();
 	    });
     }
 };
 
 int main() {
-    Reader reader;
-    Transform transform;
-    Writer writer;
-
     Pipeline pipeline;
+
+    auto reader = pipeline.create<Reader>();
+    auto transform = pipeline.create<Transform>();
+    auto writer = pipeline.create<Writer>();
+
     pipeline.connect(reader, transform);
     pipeline.connect(transform, writer);
-    reader.start();
+    pipeline.start();
 
     return 0;
 }
